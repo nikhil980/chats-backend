0e8e7db5e5774cf34277106289e46643d712c47d
nikhil980 <154825911+nikhil980@users.noreply.github.com>
2024-12-28 20:14:06 +0530

    four

--- db/connectToMongoDb.js
+++ db/connectToMongoDb.js
@@ -1,9 +1,9 @@
 import mongoose from "mongoose";
 
 const connectToMongodb=async()=>{
-       
+       const MONGO_DB_URI="mongodb+srv://user2000:plmokn980@cluster0.lcgie.mongodb.net/chat-app-db?retryWrites=true&w=majority&appName=Cluster0";
     try{
-       await mongoose.connect("mongodb+srv://user2000:plmokn980@cluster0.lcgie.mongodb.net/chat-app-db?retryWrites=true&w=majority&appName=Cluster0");
+       await mongoose.connect(MONGO_DB_URI);
        console.log("Mongodb is connected")
     }
     catch(error)
